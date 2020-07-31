dc3ba0e4bb028c4f48d460ee113f940d4a59258a
Divisekara <9asitha7@gmail.com>
2020-08-01 01:06:21 +0530

    123

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 //this is my first javascript
-console.log('asitha elaa')
+
+let name = 'asitha';
+
+console.log(name);
+
+//name cannot to be a reserved keyword
+//Variable should be meaningful
+//cannot start with a number
+//cannot contain a space or a hyphen -
+//case sensitive
+//camel notation
+
+let firstName, secondName;
+
+let thirdName='asitha', fourthName = 'diviya'
+//but the best practise is to use a single line for a single variable defining
+
+
+
