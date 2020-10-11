58b27d5304111061a714f1f833c7139c4b0e3d6a
Divisekara <9asitha7@gmail.com>
2020-10-11 18:31:43 +0530

    12

--- JavaScript/Learning/index.js
+++ JavaScript/Learning/index.js
@@ -27,9 +27,63 @@ let isApproved = true; //boolean literal
 let myFirstName = undefined; //
 Color = null;
 
+//Referrence type  - objects, arrays, functions
+let person = {  // This curcly braces stands for object literal
+    name: 'Mosh',
+    age: 30
+}
 
+console.log(person);
 
+//Accessing the keys and values
+//dot notation
 
+person.name = 'John';
 
+console.log(person.name);
 
+//bracket notation
+person['name'] = 'Mary';
+
+console.log(person);
+
+let selection = 'name';
+person[selection] = 'asitha';
+
+console.log(person);
+
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors[2]);
+selectedColors[3] = 'green';
+console.log(selectedColors.length);
+
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('asitha', 'divisekara');
+greet('thushani', 'chandrasekara');
+
+function square(number){
+    return number*number;
+}
+
+let squaredNumber  = square(10);
+console.log(squaredNumber);
+
+for (let index = 0; index < 10; index++) {
+    console.log('number =',index);
+    if(index == 0){
+        console.log('loop started');
+    }
+
+    else if(index<6){
+        console.log(index**2);
+    }
+
+    else{
+        console.log('This is else');
+    }
+    
+}
 
