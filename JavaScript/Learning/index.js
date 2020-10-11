d6404cd87380b630798082f8be0eae76293506ae
Divisekara <9asitha7@gmail.com>
2020-10-11 14:29:04 +0530

    1

--- JavaScript/Learning/index.js
+++ JavaScript/Learning/index.js
@@ -1,1 +1,35 @@
-function 
+//This is my first javascript code
+console.log("Hello world")
+
+
+//Variables name cannto be reserved keywords
+//Variable name should be meaningful 
+//Variable name cannt start with a number
+//cannot contain space or a hyphen
+//variable name is case-sensitive
+//variable name defining is more meaningful if they use camel-notation
+
+
+let name = 'asitha'; 
+let firstName = 'asitha';
+let lastname = 'divisekara';
+
+//contants
+const interestRate = 0.3;
+//we cannot reassign a contant this is wrong interestRate = 1;
+console.log(name);
+
+
+//primitives, value types - strings, boolean, null, undefined, number
+let myName  = 'asitha'; // string literal
+let age = 30; //number literal
+let isApproved = true; //boolean literal
+let myFirstName = undefined; //
+Color = null;
+
+
+
+
+
+
+
